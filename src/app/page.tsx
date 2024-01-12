aaacbc579dcb153dbbde5a9a126b452f4c74553d
Guilherme <guisarria@gmail.com>
2024-01-11 21:57:58 -0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,42 +14,42 @@ import {
   Accordion,
   AccordionContent,
   AccordionItem,
-  AccordionTrigger,
+  AccordionTrigger
 } from '@/components/ui/accordion'
 import { warnerRanking } from '@/constants'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center overflow-x-hidden'>
-      <section className='flex flex-col bg-hero bg-no-repeat bg-cover min-h-screen justify-around items-center text-center space-y-6 p-12'>
-        <Image src={Logo} alt='Logo Animativa' />
-        <div className='text-2xl sm:text-4xl sm:w-3/4 space-y-20 sm:space-y-40'>
-          <p className='font-heading'>
+    <main className="flex min-h-screen flex-col items-center overflow-x-hidden">
+      <section className="flex flex-col 2xl:w-full bg-hero bg-no-repeat bg-cover min-h-screen justify-around items-center text-center space-y-6 p-12">
+        <Image src={Logo} alt="Logo Animativa" />
+        <div className="text-2xl sm:text-4xl sm:w-3/4 space-y-20 sm:space-y-40">
+          <p className="font-heading">
             A Animativa lançou em abril de 2023 algumas campanhas para a sua
             força de vendas superar as metas e colocar ainda mais din din no
             bolso.
           </p>
-          <p className='text-custom-green font-bold'>
+          <p className="text-custom-green font-bold">
             Conheça cada uma delas e veja quem está no topo dos rankings. Ainda
             dá tempo de participar e ganhar muitos prêmios.
           </p>
         </div>
         <Image
           src={MouseIconAnimated}
-          alt='Arraste para baixo'
-          className='animate-bounce size-16'
+          alt="Arraste para baixo"
+          className="animate-bounce size-16"
         />
       </section>
-      <section className='flex flex-col bg-warner bg-no-repeat bg-cover min-h-screen justify-center p-12 sm:p-20'>
-        <div className='flex flex-col justify-around sm:flex-row'>
-          <div className='px-20 sm:px-0'>
-            <div className='flex items-center space-x-4 mb-20'>
-              <Image src={WarnerLogo} alt='Logo Warner' />
-              <h2 className='font-heading text-7xl text-custom-yellow'>
+      <section className="flex flex-col 2xl:w-full bg-warner bg-no-repeat bg-cover min-h-screen justify-center p-12 sm:p-20">
+        <div className="flex flex-col justify-around sm:flex-row">
+          <div className="px-20 sm:px-0">
+            <div className="flex items-center space-x-4 mb-20">
+              <Image src={WarnerLogo} alt="Logo Warner" />
+              <h2 className="font-heading text-7xl text-custom-yellow">
                 Warner
               </h2>
             </div>
-            <div className='text-2xl sm:text-3xl font-semibold space-y-10 [&_b]:text-custom-yellow [&_b]:mr-1 sm:[&_p]:w-1/2 text-center sm:text-left'>
+            <div className="text-2xl sm:text-3xl font-semibold space-y-10 [&_b]:text-custom-yellow [&_b]:mr-1 sm:[&_p]:w-1/2 text-center sm:text-left">
               <p>
                 <b>Ativação geral:</b>
                 atingir 4.9 milhões em vendas;
@@ -79,14 +79,14 @@ export default function Home() {
               <Image src={WarnerBookFour} alt='Livro Warner' />
             </div> */}
           </div>
-          <div className='pt-10 sm:pt-0 px-20 sm:px-0'>
-            <h2 className='font-heading text-6xl text-custom-yellow text-center sm:text-left'>
+          <div className="pt-10 sm:pt-0 px-20 sm:px-0">
+            <h2 className="font-heading text-6xl text-custom-yellow text-center sm:text-left">
               Ranking
             </h2>
             <div
-              className='text-black mt-10 space-y-4 
+              className="text-black mt-10 space-y-4 
               [&_div]:rounded-full [&_div]:w-96 [&_div]:h-12 [&_div]:bg-white [&_div]:text-center [&_div]:items-center [&_div]:flex [&_div]:justify-center 
-            [&_a]:text-black'
+            [&_a]:text-black"
             >
               {warnerRanking.map((position) => {
                 return (
@@ -99,16 +99,16 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className='flex flex-col bg-carioca bg-no-repeat bg-cover min-h-screen min-w-full justify-center p-12 sm:p-20'>
-        <div className='flex flex-col w-full justify-around sm:flex-row '>
-          <div className='px-4 sm:px-0'>
-            <div className='flex items-center space-x-4 mb-20'>
-              <Image src={CariocaLogo} alt='Logo Carioca' />
-              <h2 className='font-heading text-5xl sm:text-7xl text-custom-green'>
+      <section className="flex flex-col 2xl:w-full bg-carioca bg-no-repeat bg-cover min-h-screen min-w-full justify-center p-12 sm:p-20">
+        <div className="flex flex-col w-full justify-around sm:flex-row ">
+          <div className="px-4 sm:px-0">
+            <div className="flex items-center space-x-4 mb-20">
+              <Image src={CariocaLogo} alt="Logo Carioca" />
+              <h2 className="font-heading text-5xl sm:text-7xl text-custom-green">
                 Carioca
               </h2>
             </div>
-            <div className='text-2xl sm:text-3xl font-semibold space-y-10 [&_b]:text-custom-green [&_b]:mr-1 sm:[&_p]:w-1/2 text-center sm:text-left'>
+            <div className="text-2xl sm:text-3xl font-semibold space-y-10 [&_b]:text-custom-green [&_b]:mr-1 sm:[&_p]:w-1/2 text-center sm:text-left">
               <p>
                 <b>Ativação geral:</b>
                 atingir 10.5 milhões em vendas;
@@ -125,43 +125,43 @@ export default function Home() {
               </p>
               <p>
                 Incentivos de
-                <b className='ml-2'>Compre e ganhe:</b>
+                <b className="ml-2">Compre e ganhe:</b>
               </p>
-              <Accordion type='single' collapsible className='sm:w-2/3'>
-                <AccordionItem value='item-1'>
-                  <AccordionTrigger className='font-bold text-3xl'>
+              <Accordion type="single" collapsible className="sm:w-2/3">
+                <AccordionItem value="item-1">
+                  <AccordionTrigger className="font-bold text-3xl">
                     Clientes
                   </AccordionTrigger>
-                  <AccordionContent className='text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1'>
+                  <AccordionContent className="text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1">
                     <b>3 primeiros:</b>a cada 100 mil reais em compras, ganham
                     um IPhone 14 128gb cada;
                   </AccordionContent>
-                  <AccordionContent className='text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1'>
+                  <AccordionContent className="text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1">
                     <b>5 primeiros:</b>a cada 75 mil reais em compras, ganham
                     um notebook cada;
                   </AccordionContent>
-                  <AccordionContent className='text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1'>
+                  <AccordionContent className="text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1">
                     <b>10 primeiros:</b>a cada 50 mil reais em compras, ganham
                     um JBL, Caixa de Som Bluetooth cada;
                   </AccordionContent>
-                  <AccordionContent className='text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1'>
+                  <AccordionContent className="text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1">
                     <b>15 primeiros:</b>a cada 30 mil reais em compras, ganham
                     um Smartwatch Galaxy Samsung Watch4 Bt 40mm – Preto cada;
                   </AccordionContent>
                 </AccordionItem>
-                <AccordionItem value='item-2'>
-                  <AccordionTrigger className='font-bold text-3xl'>
+                <AccordionItem value="item-2">
+                  <AccordionTrigger className="font-bold text-3xl">
                     Time de Vendas
                   </AccordionTrigger>
-                  <AccordionContent className='text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1'>
+                  <AccordionContent className="text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1">
                     <b>3 primeiros:</b>a cada 100 mil reais em vendas, ganham um
                     IPhone 14 128gb cada;
                   </AccordionContent>
-                  <AccordionContent className='text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1'>
+                  <AccordionContent className="text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1">
                     <b>5 primeiros:</b>a cada 50 mil reais em vendas, ganham um
                     Notebook cada;
                   </AccordionContent>
-                  <AccordionContent className='text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1'>
+                  <AccordionContent className="text-2xl font-semibold [&_b]:text-custom-green [&_b]:mr-1">
                     <b>10 primeiros:</b> a cada 30 mil reais em vendas, ganham
                     uma JBL, Caixa de Som Bluetooth cada;
                   </AccordionContent>
@@ -169,14 +169,14 @@ export default function Home() {
               </Accordion>
             </div>
           </div>
-          <div className='pt-10 sm:pt-0'>
-            <h2 className='font-heading text-6xl text-custom-green text-center sm:text-left'>
+          <div className="pt-10 sm:pt-0">
+            <h2 className="font-heading text-6xl text-custom-green text-center sm:text-left">
               Ranking
             </h2>
             <div
-              className='text-black mt-10 space-y-4 items-center justify-center
+              className="text-black mt-10 space-y-4 items-center justify-center
               [&_div]:rounded-full [&_div]:w-96 [&_div]:h-12 [&_div]:bg-white [&_div]:text-center [&_div]:items-center [&_div]:flex [&_div]:justify-center 
-            [&_a]:text-black'
+            [&_a]:text-black"
             >
               <div>
                 <a>Ranking</a>
@@ -212,13 +212,13 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className='flex flex-col bg-faturamento bg-no-repeat bg-cover min-h-screen justify-center items-center p-12 sm:p-20'>
-        <div className='flex flex-col justify-around sm:flex-row'>
-          <div className='px-20 sm:px-0'>
-            <h2 className='font-heading text-6xl text-custom-pink text-center sm:text-left mb-10'>
+      <section className="flex flex-col 2xl:w-full bg-faturamento bg-no-repeat bg-cover min-h-screen justify-center items-center p-12 sm:p-20">
+        <div className="flex flex-col justify-around sm:flex-row">
+          <div className="px-20 sm:px-0">
+            <h2 className="font-heading text-6xl text-custom-pink text-center sm:text-left mb-10">
               fatuRamento
             </h2>
-            <div className='text-2xl sm:text-3xl font-semibold space-y-10 text-custom-purple [&_b]:text-custom-pink [&_b]:mr-1 sm:[&_p]:w-1/2'>
+            <div className="text-2xl sm:text-3xl font-semibold space-y-10 text-custom-purple [&_b]:text-custom-pink [&_b]:mr-1 sm:[&_p]:w-1/2">
               <p>
                 <b>Ativação geral:</b>
                 atingir 260.1 milhões em vendas (90% do objetivo da companhia do
@@ -232,15 +232,15 @@ export default function Home() {
                 Atingir de 90 a 99.99% - 1 pontos
               </p>
               <Accordion
-                type='single'
+                type="single"
                 collapsible
-                className='sm:w-1/2 space-y-4'
+                className="sm:w-1/2 space-y-4"
               >
-                <AccordionItem value='item-1' className='border-custom-purple'>
-                  <AccordionTrigger className='font-bold text-3xl text-custom-pink text-left'>
+                <AccordionItem value="item-1" className="border-custom-purple">
+                  <AccordionTrigger className="font-bold text-3xl text-custom-pink text-left">
                     Desconto concedido em negociações de vendas
                   </AccordionTrigger>
-                  <AccordionContent className='text-2xl font-semibold'>
+                  <AccordionContent className="text-2xl font-semibold">
                     Cálculo: A composição será feita considerando a RL para
                     nivelar os preços entre os estados. Com isso, será feita a
                     composição total da venda realizada versus o valor total de
@@ -249,11 +249,11 @@ export default function Home() {
                     De 35.1 a 42% - 1 ponto
                   </AccordionContent>
                 </AccordionItem>
-                <AccordionItem value='item-2' className='border-custom-purple'>
-                  <AccordionTrigger className='font-bold text-3xl text-custom-pink text-left'>
+                <AccordionItem value="item-2" className="border-custom-purple">
+                  <AccordionTrigger className="font-bold text-3xl text-custom-pink text-left">
                     Positivação por categoria de produto
                   </AccordionTrigger>
-                  <AccordionContent className='text-2xl font-semibold pl-4'>
+                  <AccordionContent className="text-2xl font-semibold pl-4">
                     <ul>
                       <li>Commodity</li>
                       <li>Marca própria</li>
@@ -262,42 +262,42 @@ export default function Home() {
                       <li>Agendas/Planners</li>
                     </ul>
                   </AccordionContent>
-                  <AccordionContent className='text-2xl font-semibold'>
+                  <AccordionContent className="text-2xl font-semibold">
                     · Se 90% dos clientes da base comprarem todas as categorias
                     participantes - 2 pontos
                   </AccordionContent>
-                  <AccordionContent className='text-2xl font-semibold'>
+                  <AccordionContent className="text-2xl font-semibold">
                     · Se 95% dos clientes da base comprarem todas as categorias
                     participantes - 3 pontos
                   </AccordionContent>
-                  <AccordionContent className='text-2xl font-semibold'>
+                  <AccordionContent className="text-2xl font-semibold">
                     · Se 100% dos clientes da base comprarem todas as categorias
                     participantes - 5 pontos
                   </AccordionContent>
                 </AccordionItem>
-                <AccordionItem value='item-3' className='border-custom-purple'>
-                  <AccordionTrigger className='font-bold text-3xl text-custom-pink text-left'>
+                <AccordionItem value="item-3" className="border-custom-purple">
+                  <AccordionTrigger className="font-bold text-3xl text-custom-pink text-left">
                     Acompanhamento de PDVs
                   </AccordionTrigger>
-                  <AccordionContent className='text-2xl font-semibold'>
+                  <AccordionContent className="text-2xl font-semibold">
                     Calculo: Quantidade de PDVs (fotos enviadas pelo cliente) /
                     Base de clientes Ativa
                   </AccordionContent>
                 </AccordionItem>
-                <AccordionItem value='item-4' className='border-custom-purple'>
-                  <AccordionTrigger className='font-bold text-3xl text-custom-pink text-left'>
+                <AccordionItem value="item-4" className="border-custom-purple">
+                  <AccordionTrigger className="font-bold text-3xl text-custom-pink text-left">
                     Critério final
                   </AccordionTrigger>
-                  <AccordionContent className='text-2xl font-semibold'>
+                  <AccordionContent className="text-2xl font-semibold">
                     Soma das pontuações:
                     <br /> · Se 90% dos clientes da base comprarem todas as
                     categorias participantes - 2 pontos
                   </AccordionContent>
-                  <AccordionContent className='text-2xl font-semibold'>
+                  <AccordionContent className="text-2xl font-semibold">
                     · Se 95% dos clientes da base comprarem todas as categorias
                     participantes - 3 pontos
                   </AccordionContent>
-                  <AccordionContent className='text-2xl font-semibold'>
+                  <AccordionContent className="text-2xl font-semibold">
                     · Se 100% dos clientes da base comprarem todas as categorias
                     participantes - 5 pontos
                   </AccordionContent>
@@ -318,14 +318,14 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className='pt-10 sm:pt-0 px-20 sm:px-0'>
-            <h2 className='font-heading text-6xl text-custom-purple text-center sm:text-left'>
+          <div className="pt-10 sm:pt-0 px-20 sm:px-0">
+            <h2 className="font-heading text-6xl text-custom-purple text-center sm:text-left">
               Ranking
             </h2>
             <div
-              className='text-black mt-10 space-y-4 
+              className="text-black mt-10 space-y-4 
               [&_div]:rounded-full [&_div]:w-96 [&_div]:h-12 [&_div]:bg-white [&_div]:text-center [&_div]:items-center [&_div]:flex [&_div]:justify-center 
-            [&_a]:text-black'
+            [&_a]:text-black"
             >
               <div>
                 <a>Ranking</a>
