6af5757bd76cd90494c0b3f292864214862a17d0
guimaya <148132668+guimaya@users.noreply.github.com>
2024-02-19 08:49:26 -0300

    ranking

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,37 +1,37 @@
 export const warnerRanking = [
-  { name: 'M DO S A DE SOUZA REPRESENTACOES', position: 1 },
-  { name: 'GRAFIT REPRESENTACOES COMERCIAIS', position: 2 },
-  { name: 'SCHLEU SERRA REPRESENTACOES', position: 3 },
-  { name: 'SUL BRINDES REPRESENTACOES LTDA', position: 4 },
-  { name: 'ELIANE DO NASCIMENTO CRUZ', position: 5 },
-  { name: 'J K REPRESENTACOES LTDA', position: 6 },
-  { name: 'FACCURY REPRESENTACOES DE', position: 7 },
-  { name: 'PLENA REPRESENTACOES SC LTDA ME', position: 8 },
-  { name: 'P E H PAPER REPRESENTACOES LTDA ME', position: 9 },
-  { name: 'ORGANIZACAO AVA VITORIA', position: 10 }
+  { name: 'HELTON LAURENCO DE BARROS', position: 1 },
+  { name: 'MARCONDES & MARCONDES REPRESENTACAO', position: 2 },
+  { name: 'GRAFIT REPRESENTACOES COMERCIAIS', position: 3 },
+  { name: 'M DO S A DE SOUZA REPRESENTACOES', position: 4 },
+  { name: 'SCHLEU SERRA REPRESENTACOES', position: 5 },
+  { name: 'POSITIVACAO REPRESENTACOES EIRELI', position: 6 },
+  { name: 'PLENA REPRESENTACOES SC LTDA ME', position: 7 },
+  { name: 'J K REPRESENTACOES LTDA', position: 8 },
+  { name: 'ORGANIZACAO AVA VITORIA', position: 9 },
+  { name: 'SUL BRINDES REPRESENTACOES LTDA', position: 10 }
 ]
 export const cariocaRanking = [
   { name: 'BMP UTILIDADES DOMESTICAS S A', position: 1 },
   { name: 'ANDERSON SCHOENAU CARDOSO', position: 2 },
   { name: 'LIDER COMERCIO E IND LTDA', position: 3 },
-  { name: 'LOJAS TEM VARIEDADES LTDA', position: 4 },
-  { name: 'LEITURA CAMPINAS LTDA', position: 5 },
-  { name: 'GB COMERCIO E SERVICOS LTDA', position: 6 },
-  { name: 'MEDICAL DA AMAZONIA EIREL', position: 7 },
-  { name: 'PAPEL E CIA PAPELARIA PRO', position: 8 },
-  { name: 'TEND TUDO BAZAR COMERCIO ATACADISTA', position: 9 },
-  { name: 'ESTILO LIVRARIA E PAPELARIA LTDA', position: 10 }
+  { name: 'GB COMERCIO E SERVICOS LTDA', position: 4 },
+  { name: 'LOJAS TEM VARIEDADES LTDA', position: 5 },
+  { name: 'DARNY MENDES PICANCO 00098505203', position: 6 },
+  { name: 'LEITURA CAMPINAS LTDA', position: 7 },
+  { name: 'PRIVALIA BRASIL S.A', position: 8 },
+  { name: 'MEDICAL DA AMAZONIA EIREL', position: 9 },
+  { name: 'PAPEL E CIA PAPELARIA PRO', position: 10 }
 ]
 
 export const faturamentoRanking = [
   { name: 'LAIZ OLIVEIRA', position: 1 },
-  { name: 'PATRICIA SALLES', position: 2 },
-  { name: 'ISABELLA BERNARDES', position: 3 },
-  { name: 'L&C REPRESENTACOES LTDA', position: 4 },
-  { name: 'A VILLA REPRESENTACOES LTDA ME', position: 5 },
+  { name: 'J H FERNANDES BALDIM', position: 2 },
+  { name: 'A VILLA REPRESENTACOES LTDA ME', position: 3 },
+  { name: 'M PINHEIRO COMERCIO E REPRESENTACOE', position: 4 },
+  { name: 'SAUANDAG REPRESENTACOES LTDA', position: 5 },
   { name: 'DANIEL DE SOUSA', position: 6 },
-  { name: 'ANDREIA DA SILVA NASCIMENTO', position: 7 },
-  { name: 'M PINHEIRO COMERCIO E REPRESENTACOE', position: 8 },
-  { name: 'SAUANDAG REPRESENTACOES LTDA', position: 9 },
-  { name: 'IZABELA VICENTIM', position: 10 }
+  { name: 'ROBERTO SOUZA E CIA LTDA', position: 7 },
+  { name: 'ANDREIA DA SILVA NASCIMENTO', position: 8 },
+  { name: 'IZABELA VICENTIM', position: 9 },
+  { name: 'ISABELLA BERNARDES', position: 10 }
 ]
