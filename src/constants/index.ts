d697ab18dbe353bbe1791c6604ac27694c2382a6
guimaya <148132668+guimaya@users.noreply.github.com>
2024-03-14 09:34:12 -0300

    ranking

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -14,11 +14,11 @@ export const cariocaRanking = [
   { name: 'BMP UTILIDADES DOMESTICAS S A', position: 1 },
   { name: 'ANDERSON SCHOENAU CARDOSO', position: 2 },
   { name: 'LIDER COMERCIO E IND LTDA', position: 3 },
-  { name: 'GB COMERCIO E SERVICOS LTDA', position: 4 },
-  { name: 'LOJAS TEM VARIEDADES LTDA', position: 5 },
-  { name: 'DARNY MENDES PICANCO 00098505203', position: 6 },
-  { name: 'LEITURA CAMPINAS LTDA', position: 7 },
-  { name: 'PRIVALIA BRASIL S.A', position: 8 },
+  { name: 'LEITURA CAMPINAS LTDA', position: 4 },
+  { name: 'GB COMERCIO E SERVICOS LTDA', position: 5 },
+  { name: 'LOJAS TEM VARIEDADES LTDA', position: 6 },
+  { name: 'PRIVALIA BRASIL S.A', position: 7 },
+  { name: 'DARNY MENDES PICANCO 00098505203', position: 8 },
   { name: 'MEDICAL DA AMAZONIA EIREL', position: 9 },
   { name: 'PAPEL E CIA PAPELARIA PRO', position: 10 }
 ]
@@ -26,12 +26,12 @@ export const cariocaRanking = [
 export const faturamentoRanking = [
   { name: 'LAIZ OLIVEIRA', position: 1 },
   { name: 'SAUANDAG REPRESENTACOES LTDA', position: 2 },
-  { name: 'DANIEL DE SOUSA', position: 3 },
+  { name: 'ANDURAND COMERCIO E REPRESENTACOES', position: 3 },
   { name: 'OC INOVA REPRESENTACAO EIRELI', position: 4 },
   { name: 'M PINHEIRO COMERCIO E REPRESENTACOE', position: 5 },
-  { name: 'E VERA CRUZ DA SILVA REPRESENTACOES', position: 6 },
-  { name: 'IZABELA VICENTIM', position: 7 },
-  { name: 'ROBERTO SOUZA E CIA LTDA', position: 8 },
-  { name: 'ANDREIA DA SILVA NASCIMENTO', position: 9 },
-  { name: 'MAROPEL REPRESENTACOES', position: 10 }
+  { name: 'DANIEL DE SOUSA', position: 6 },
+  { name: 'ANDREIA DA SILVA NASCIMENTO', position: 7 },
+  { name: 'P E H PAPER REPRESENTACOES LTDA ME', position: 8 },
+  { name: 'E VERA CRUZ DA SILVA REPRESENTACOES', position: 9 },
+  { name: 'ROBERTO SOUZA E CIA LTDA', position: 10 }
 ]
